b252dab84f518a526809d4de4da91e780265bc19
Manan-1112 <mananbansal449@gmail.com>
2026-03-08 23:11:22 +0530

    added github Oauth

---
+++ Backend/oauth.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+const GithubStrategy = require('passport-github2').Strategy;
+module.exports = passport.use(new GithubStrategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+  console.log("Access Token:", accessToken);
+  console.log("Profile:", profile);
+
+  return done(null, profile);
+}));
