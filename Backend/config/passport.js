bae14bfa1403b686524ebf9ce604ec2fd510adf6
Manan-1112 <mananbansal449@gmail.com>
2026-03-10 21:52:09 +0530

    serialized/deserialized the user before

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -1,14 +1,24 @@
 const passport = require('passport');
 const GithubStrategy = require('passport-github2').Strategy;
 
-
-console.log(process.env.clientID,);
-module.exports = passport.use(new GithubStrategy({
+passport.use(new GithubStrategy({
     clientID: process.env.clientID,
     clientSecret: process.env.clientSecret,
     callbackURL: 'http://localhost:3000/auth/github/callback'
   },
   function(accessToken, refreshToken, profile, done) {
+    console.log("ACCESS TOKEN:", accessToken);
+    console.log("PROFILE:", profile);
 
   return done(null, profile);
 }));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+module.exports = passport;
